1575c14189485f797db29f1323db3281cb8df1c2
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2023-12-03 21:58:35 -0300

    structure: :art: | rename "lang" occurrences to "locale" in order to keep it consistent throughout the project .

--- app/components/widgets/localeSwitcher/index.tsx
+++ app/components/widgets/localeSwitcher/index.tsx
@@ -7,20 +7,20 @@ import { usePathname, useRouter } from "next/navigation";
 import { Select, SelectItem } from "@nextui-org/react";
 
 // Todo: () => Create a file for this custom hook
-const useCurrentLang = () => {
+const useCurrentLocale = () => {
   const pathName = usePathname();
 
   return pathName.split("/")[1];
 };
 
 export const LocaleSwitcher = () => {
-  const currentLang = useCurrentLang();
+  const currentLocale = useCurrentLocale();
   const pathName = usePathname();
   const router = useRouter();
 
-  const formattedLanguages = i18n.locales.map((lang) => ({
-    value: lang,
-    label: lang,
+  const formattedLocales = i18n.locales.map((locale) => ({
+    value: locale,
+    label: locale,
   }));
 
   const redirectedPathName = (locale: string) => {
