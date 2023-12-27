df21aa49649959c0add33bf13485f77d4b536871
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2023-12-27 09:56:48 -0300

    feat: :sparkles: | create a LogoGray icon that will be used in the home footer section.

--- app/components/widgets/icons/index.ts
+++ app/components/widgets/icons/index.ts
@@ -1,5 +1,5 @@
 export { LogoStandard } from "./brand/logos/standard";
-export { Logo } from "./logo";
+export { LogoGray } from "./brand/logos/gray";
 
 export { TwitterIcon } from "./social-media/twitter";
 export { GithubIcon } from "./social-media/github";
