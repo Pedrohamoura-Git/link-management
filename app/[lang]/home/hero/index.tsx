d60284151a9a35130eb8ba3a54eca4e808134fc8
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-01-02 06:37:58 -0300

    :art: | style: places the hero content size by size on mid size screens.

--- app/[lang]/home/hero/index.tsx
+++ app/[lang]/home/hero/index.tsx
@@ -19,26 +19,33 @@ type heroProps = {
 export const Hero = ({ lang, heroLocale }: heroProps) => {
   return (
     <section className={`${section}`}>
-      <div className="flex flex-col items-center justify-center gap-6">
+      <div className="flex flex-col items-center justify-center gap-6 md:flex-row-reverse md:justify-around">
         <Image
+          className="md:basis-96"
           src={headerImage}
           alt="Image of a smartphone with links to share"
         />
 
-        <h1 className="text-4xl text-center">{heroLocale.hero.title}</h1>
+        <div className="flex flex-col items-center justify-center gap-6 md:basis-1/3 md:items-start">
+          <h1 className="text-4xl text-center md:text-left">
+            {heroLocale.hero.title}
+          </h1>
 
-        <p className="px-2 text-center">{heroLocale.hero.description}</p>
+          <p className="px-2 text-center md:text-xl md:text-left">
+            {heroLocale.hero.description}
+          </p>
 
-        <Button
-          className="text-1xl p-7"
-          href={`/${lang}`}
-          as={Link}
-          color="primary"
-          showAnchorIcon
-          variant="solid"
-        >
-          {heroLocale.hero.cta.try_now}
-        </Button>
+          <Button
+            className="text-1xl p-7"
+            href={`/${lang}`}
+            as={Link}
+            color="primary"
+            showAnchorIcon
+            variant="solid"
+          >
+            {heroLocale.hero.cta.try_now}
+          </Button>
+        </div>
       </div>
     </section>
   );
