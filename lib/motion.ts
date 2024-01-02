5af59d6a60d2926b7f232573f643f3b1586d9b89
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-01-02 05:24:30 -0300

    :construction: WIP: exports motion instance, motion animations and some basic html tags from a single client file in order to avoid issues with SSR .

---
+++ lib/motion.ts
@@ -0,0 +1,8 @@
+"use client";
+
+import { motion } from "framer-motion";
+import * as animations from "@/lib/motionAnimations";
+
+export const animation = animations;
+export const section = motion.section;
+export const div = motion.div;
