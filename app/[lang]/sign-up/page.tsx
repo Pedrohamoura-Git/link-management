1008fa22760348ced75a41b947da88b7f7102a7e
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-02-04 17:34:05 -0300

    ✨ | feat: adds header logo and title in the sign up page..

--- app/[lang]/sign-up/page.tsx
+++ app/[lang]/sign-up/page.tsx
@@ -1,8 +1,13 @@
 import { RegisterForm } from "@@/components";
+import { LogoWithCompanyOwner } from "@@/components/widgets";
 
 export default function signUp() {
   return (
     <div className="relative -translate-x-1/2 translate-y-[10%] left-1/2 flex flex-col items-center justify-center">
+      <header className="mb-10">
+        <LogoWithCompanyOwner width={175} height={50} />
+        <h1 className="mt-5">Crie sua conta Linka.ai gratuitamente</h1>
+      </header>
 
       <RegisterForm />
     </div>
