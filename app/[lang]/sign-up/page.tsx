7bc193c3815f8a61e48aacd621e91a46ce9aced5
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-02-04 17:32:19 -0300

    ✨ | feat: creates the sign up page with the registerFom.

---
+++ app/[lang]/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterForm } from "@@/components";
+
+export default function signUp() {
+  return (
+    <div className="relative -translate-x-1/2 translate-y-[10%] left-1/2 flex flex-col items-center justify-center">
+
+      <RegisterForm />
+    </div>
+  );
+}
