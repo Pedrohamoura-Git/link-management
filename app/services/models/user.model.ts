d0841390630dc2937aa548276e847779d944eb98
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-02-06 04:58:54 -0300

    🗃️ | database: create a mongoose user`s schema.

---
+++ app/services/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
