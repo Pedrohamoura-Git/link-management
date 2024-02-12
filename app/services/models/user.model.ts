7720e1bfc513333d42ded359f42fcb18475ca57f
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-02-12 16:14:27 -0300

    🗃️ | database: add creation and update date in the user's schema.

--- app/services/models/user.model.ts
+++ app/services/models/user.model.ts
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  creationDate: { type: Date, default: Date.now },
+  updateDate: { type: Date },
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
