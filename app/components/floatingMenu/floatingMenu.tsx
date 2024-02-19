68f895289f0034af34646de7f0845e4362232acd
Pedro Moura <pedro.h.a.moura.business@gmail.com>
2024-02-19 06:06:18 -0300

    🚧 | WIP: create an arrow icon and refs for the wrapper and the arrow so their movement can be tracked in the screen.

--- app/components/floatingMenu/floatingMenu.tsx
+++ app/components/floatingMenu/floatingMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import {
   Dropdown,
   Link,
@@ -12,26 +12,58 @@ import {
 import { ThemeSwitcher, LocaleSwitcher } from "../widgets";
 
 export const FloatingMenu = () => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const arrowRef = useRef<HTMLDivElement>(null);
+  const wrapperCoords = useRef<{
+    startX: number;
+    startY: number;
+    lastX: number;
+    lastY: number;
+  }>({
+    startX: 0,
+    startY: 0,
+    lastX: 0,
+    lastY: 0,
+  });
   return (
-    <div className="fixed left-0 z-10 bottom-unit-5xl">
-      <Dropdown
-        backdrop="blur"
-        className="border bg-background border-border"
-        closeOnSelect={false}
-      >
-        <DropdownTrigger>
-          <Button variant="bordered">Open Menu</Button>
-        </DropdownTrigger>
+    <div
+      className="fixed left-0 z-10 hover:cursor-pointer bottom-unit-5xl"
+      ref={wrapperRef}
+    >
+        <Dropdown
+          backdrop="blur"
+          className="border bg-background border-border"
+          closeOnSelect={false}
+        >
+          <DropdownTrigger>
+            <Button variant="bordered">Open Menu</Button>
+          </DropdownTrigger>
 
-        <DropdownMenu variant="faded" aria-label="Static Actions">
-          <DropdownItem key="theme-switcher">
-            <ThemeSwitcher />
-          </DropdownItem>
-          <DropdownItem key="locale-switcher">
-            <LocaleSwitcher />
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+          <DropdownMenu variant="faded" aria-label="Static Actions">
+            <DropdownItem key="theme-switcher">
+              <ThemeSwitcher />
+            </DropdownItem>
+            <DropdownItem key="locale-switcher">
+              <LocaleSwitcher />
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+
+        <i
+          ref={arrowRef}
+          className="arrow right"
+          style={{
+            height: "25px",
+            width: "25px",
+            border: "solid red",
+            borderWidth: "0 3px 3px 0",
+            display: "inline-block",
+            // padding: "3px",
+            transform: "rotate(-45deg)",
+            // -webkit-transform: rotate(-45deg),
+          }}
+        ></i>
+      </div>
     </div>
   );
 };
